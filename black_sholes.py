c2332d77f74a0c5d33ac6f489eecf034cd22a612
samuelrosen111 <samuelrosen121@gmail.com>
2023-02-18 10:45:25 +0100

    Clear much better version

--- black_sholes.py
+++ black_sholes.py
@@ -30,7 +30,6 @@ def cumulative_prob(x): #cumulative_probability_standard_normal_distribution mu=
         
     return cumulative
 
-
 def gbm(S0, mu, sigma, T, N):
     """
     Generate a Geometric Brownian Motion process.
@@ -53,9 +52,96 @@ def gbm(S0, mu, sigma, T, N):
     S = S0 * np.exp(X)
     return S
 
-if(0):
-    for i in np.arange(-3.1, 3.1, 0.1):
-        print("Probability that value is: ", round(i,0), " (or above) is: ", round(cumulative_prob(i), 2))
+def illustrate_gbm():
+    # Set initial stock price, expected return, volatility, time, and number of steps
+    S0 = 100 # Initial stock price
+    mu = 0.1 # Expected value
+    sigma = 0.2 #standard deviation of returns
+    T = 1 # Total time of simulation
+    N = 252 # Number of discrete time-steps in the simulation
+
+    print(f"Illustration of GBM with parameters: \nInitial stock price: {S0} \nExpected return: {mu} \nVolatility: {sigma} \nTotal time: {T} \nDiscrete time steps: {N}")
+
+    # Simulate the GBM process
+    ammount_simulations = int(input("Enter how many simulation you want to run: "))
+
+    for _ in range(1, ammount_simulations+1):
+        current_GBM = gbm(S0, mu, sigma, T, N)
+        plt.plot(current_GBM)
+
+    #plt.plot(A) ghjgjgv
+    plt.xlabel("Time")
+    plt.ylabel("Stock Price")
+    plt.title("Geometric Brownian Motion Simulation(s)")
+    plt.show()
+    # Calculate the average of all simulations
+    average = np.mean(np.array([gbm(S0, mu, sigma, T, N) for _ in range(ammount_simulations)]), axis=0)
+    # Plot the average of all simulations
+    plt.plot(average, label='Average')
+    plt.legend()
+    plt.xlabel("Time")
+    plt.ylabel("Stock Price")
+    plt.title("Average of the Geometric Brownian Motion Simulations.")
+    plt.show()
+    simulation_numbers = [5, 10, 50, 100, 500, 1000]
+    for ammount_simulations in simulation_numbers:
+        # Calculate the average of all simulations
+        average = np.mean(np.array([gbm(S0, mu, sigma, T, N) for _ in range(ammount_simulations)]), axis=0)
+        
+        # Plot the average of all simulations
+        plt.plot(average, label=f'Average of {ammount_simulations} simulations.')
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Stock Price")
+        plt.title("Average of Geometric Brownian Motion Simulations")
+        plt.show()
+
+    # Illustrate the effect of changing expected return
+
+    mu_values = [0.05, 0.4, 0.8, 1]
+    for mu in mu_values:
+        average = np.mean(np.array([gbm(S0, mu, sigma, T, N) for _ in range(ammount_simulations)]), axis=0)
+        plt.plot(average, label=f'Expected return: {mu}')
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Stock Price")
+        plt.title("Effect of Changing Expected Return (mu)")
+    print(f"\n \n Illustration of GBM (changing mu) with parameters: \nInitial stock price: {S0} \nExpected return: {mu} \nVolatility: {sigma} \nTotal time: {T} \nDiscrete time steps: {N}")
+    plt.show()
+
+    # Illustrate the effect of changing volatility
+    sigma_values = [0, 2, 4, 8]
+    for sigma in sigma_values:
+        average = np.mean(np.array([gbm(S0, mu, sigma, T, N) for _ in range(ammount_simulations)]), axis=0)
+        plt.plot(average, label=f'Volatility: {sigma}')
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Stock Price")
+        plt.title("Effect of Changing Volatility (sigma)")
+    print(f"\n \n Illustration of GBM (changing sigma) with parameters: \nInitial stock price: {S0} \nExpected return: {mu} \nVolatility: {sigma} \nTotal time: {T} \nDiscrete time steps: {N}")
+    print("Higher volatility = lower expected value over time. Somewhere around 3 things get out of hand.")
+    plt.show()
+
+def illustrate_cumulative_prob():
+    x = np.linspace(-3, 3, 1000)
+    y_cumulative = []
+    y_normal = []
+    for i in x:
+        y_cumulative.append(float(1-cumulative_prob(i)))
+        y_normal.append(float((1 / (math.sqrt(2 * math.pi))) * math.exp(-0.5 * i * i)))
+    fig, ax1 = plt.subplots()
+    ax1.plot(x, y_cumulative, color='red')
+    ax1.set_xlabel('X Value')
+    ax1.set_ylabel('Cumulative Probability', color='red')
+    ax1.tick_params(axis='y', labelcolor='red')
+    
+    ax2 = ax1.twinx()
+    ax2.plot(x, y_normal, color='blue')
+    ax2.set_ylabel('Normal Distribution', color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+    
+    plt.title("Cumulative Probability and Normal Distribution")
+    plt.show()
 
 def black_scholes(S0, K, r, sigma, T, option_type):
     """
@@ -84,62 +170,82 @@ def black_scholes(S0, K, r, sigma, T, option_type):
         
     return value
 
-# Example 1
-S0 = 100 # initial (current) stock price
-K = 95 # strike price
-r = 0.05 # risk-free interest rate
-sigma = 0.2 # volatility of underlying asset
-T = 1 # time to maturity normalized to 1 (for ex 1 year)
-option_type = "call"
-price_call = black_scholes(S0, K, r, sigma, T, option_type)
-print(f"The price of the European {option_type} option is: {price_call}")
+def illustrate_black_scholes():
+    # Example 1
+    S0 = 100 # initial (current) stock price
+    K = 95 # strike price
+    r = 0.05 # risk-free interest rate
+    sigma = 0.2 # volatility of underlying asset
+    T = 1 # time to maturity normalized to 1 (for ex 1 year)
+    option_type = "call"
+    price_call = black_scholes(S0, K, r, sigma, T, option_type)
+    print(f"\nExample 1, for the given values: \nS0 = {S0} \nK = {K} \nr = {r} \nsigma = {sigma} \nT = {T}")
+    print(f"\nThe price of the European {option_type} option is: {price_call}")
 
-# Example 2: Calculate the price of a European put option
-S0 = 100 # initial (current) stock price
-K = 105 # strike price
-r = 0.05 # risk-free interest rate
-sigma = 0.2 # volatility of underlying asset
-T = 1 # time to maturity normalized to 1 (for ex 1 year)
-option_type = 'put'
-price_put = black_scholes(S0, K, r, sigma, T, option_type)
-print("The price of the European {option_type} option is:", price_put)
+    # Example 2: Calculate the price of a European put option
+    S0 = 100 # initial (current) stock price
+    K = 105 # strike price
+    r = 0.05 # risk-free interest rate
+    sigma = 0.2 # volatility of underlying asset
+    T = 1 # time to maturity normalized to 1 (for ex 1 year)
+    option_type = 'put'
+    price_put = black_scholes(S0, K, r, sigma, T, option_type)
+    print(f"The price of the European {option_type} option is:", price_put)
 
+    time.sleep(10)
 
-#New input values for the graph of put and call options
+    #New input values for the graph of put and call options
 
-S0 = 50 # initial (current) stock price
-K_call = 55 # strike price for call option
-K_put = 30 # strike price for put option
-r = 0.05 # risk-free interest rate
-sigma = 0.2 # volatility of underlying asset
+    S0 = 50 # initial (current) stock price
+    K_call = 55 # strike price for call option
+    K_put = 30 # strike price for put option
+    r = 0.05 # risk-free interest rate
+    sigma = 0.2 # volatility of underlying asset
 
-#time values to calculate the prices
-time_values = np.arange(0.0, 10, 0.1)
-call_prices = []
-put_prices = []
+    #time values to calculate the prices
+    time_values = np.arange(0.0, 10, 0.1)
+    call_prices = []
+    put_prices = []
 
-#calculating the prices for both call and put options
-for T in time_values:
-    call_price = -black_scholes(S0, K_call, r, sigma, T, "call")
-    call_prices.append(call_price)
-    put_price = -black_scholes(S0, K_put, r, sigma, T, "put")
-    put_prices.append(put_price)
+    #calculating the prices for both call and put options
+    for T in time_values:
+        call_price = -black_scholes(S0, K_call, r, sigma, T, "call")
+        call_prices.append(call_price)
+        put_price = -black_scholes(S0, K_put, r, sigma, T, "put")
+        put_prices.append(put_price)
 
-# Plotting the prices for call and put options over time to maturity
-""""
-plt.plot(time_values, call_prices, label="European call option")
-plt.plot(time_values, put_prices, label="European put option")
-plt.xlabel("Time to maturity (in years)")
-plt.ylabel("Option price")
-plt.legend()
-plt.show()
-"""
+    plt.plot(time_values, call_prices, label="European call option")
+    plt.plot(time_values, put_prices, label="European put option")
+    plt.xlabel("Time to maturity (in years)")
+    plt.ylabel("Option price")
+    plt.legend()
+    plt.show()
 
-# Printing the prices for put options at different time to maturity
-if(0):
-    for t, put_price in zip(time_values, put_prices):
-        print(f"Time T (in years) = {round(t,1)} Put price at this time: {round(put_price, 1)}")
+    # Printing the prices for put options at different time to maturity
+    if(1):
+        for t, put_price in zip(time_values, put_prices):
+            print(f"Time T (in years) = {round(t,1)} Put price at this time: {round(put_price, 1)}")
 
+illustrate_black_scholes()
+
+def main():
+    print("Enter what you want to do:")
+    print("1. Illustrate Cumulative Probability of Normal Distribution")
+    print("2. Illustrate Geometric Brownian Motion example runs")
+    print("3 Illustrate Black Scholes")
+    print("Any other number to skip")
+
+    test = int(input("Enter the number of the test you would like to run: "))
+    if test == 1:
+        illustrate_cumulative_prob()
+    elif test == 2:
+        illustrate_gbm()
+    elif test == 3:
+        illustrate_black_scholes()
+    else:
+        print("")
+
+    print("The end.")
 
 def simulate_options(S0, K_call, K_put, r, sigma, T, N, num_simulations):
     """
@@ -176,6 +282,14 @@ def simulate_options(S0, K_call, K_put, r, sigma, T, N, num_simulations):
     
     return mean_call_price, mean_put_price
 
+
+
+#main()
+
+
+
+
+"""""
 # Example input values
 S0 = 50
 K_call = 55
@@ -191,10 +305,10 @@ num_simulations = 1000
 option_prices = simulate_options(S0, K_call, K_put, r, sigma, T, N, num_simulations)
 
 
-
 # Plot a histogram of the simulated option prices
 plt.hist(option_prices, bins=50, edgecolor='black')
 plt.xlabel("Option price")
 plt.ylabel("Frequency")
 plt.title(f"Histogram of simulated {option_type} option prices")
 plt.show()
+"""
