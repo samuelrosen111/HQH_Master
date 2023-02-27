065d613128d8e21ada835b83a557d4034b373152
samuelrosen111 <samuelrosen121@gmail.com>
2023-02-27 14:00:47 +0100

    FRESH new look

---
+++ test_area.py
@@ -0,0 +1,15 @@
+import math
+import time
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+
+num_sims = 20
+random_number = np.random.standard_normal(num_sims)
+
+for i in random_number:
+    print(i)
+
+
+print("LEN  ", len(random_number))
